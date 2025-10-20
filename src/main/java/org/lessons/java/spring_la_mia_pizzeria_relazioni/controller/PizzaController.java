398d0452e04705a04a613ce9db90351df65b5676
arielmusa <arielmusainfo@gmail.com>
2025-10-20 11:47:26 +0200

    feat: create get route in PizzaController to /create-offer

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/PizzaController.java
@@ -3,7 +3,9 @@ package org.lessons.java.spring_la_mia_pizzeria_relazioni.controller;
 import java.util.List;
 
 import org.lessons.java.spring_la_mia_pizzeria_relazioni.model.Pizza;
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.model.SpecialOffers;
 import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.PizzaRepository;
+/* import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.SpecialOffersRepository; */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
-
 
 
 @Controller
@@ -28,6 +26,9 @@ public class PizzaController {
     
     @Autowired
     private PizzaRepository pizzaRepository;
+
+/*     @Autowired
+    private SpecialOffersRepository specialOffersRepository; */
 
     @GetMapping("")
     public String index(Model model) {
@@ -80,6 +81,18 @@ public class PizzaController {
         pizzaRepository.deleteById(id);
         return "redirect:/pizzas";
     }
+
+
+    @GetMapping("/{id}/create-offer")
+    public String createOffer(@PathVariable int id, Model model) {
+        model.addAttribute("pizza", pizzaRepository.findById(id).get());
+        SpecialOffers newOffer = new SpecialOffers();
+        newOffer.setPizza(pizzaRepository.findById(id).get());
+        model.addAttribute("offer", newOffer);
+        return "pizzas/create-offer";
+    }
+    
+  
     
 
     
